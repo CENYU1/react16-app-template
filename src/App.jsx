a79896d22629ceebd0fdb57d0e023eef01e12336
cenyu02 <cenyu02@meituan.com>
2024-02-27 11:43:16 +0800

    feat: 打开控制台看看vdom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 function App() {
-  return (
+  const vdom = (
     <div className="app">
       <h1>MamaGuide says hello 2 u</h1>
       <p className="mamaguide">Let's explore react together</p>
     </div>
   )
+  console.log(vdom)
+  return vdom
 }
 
 export default App
