40169656ce8cb97d32c184bff0a3e99618319d06
cenyu02 <cenyu02@meituan.com>
2024-02-27 11:32:27 +0800

    feat: template complete

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>MamaGuide says hello 2 u</h1>
+      <p className="mamaguide">Let's explore react together</p>
+    </div>
+  )
+}
+
+export default App
